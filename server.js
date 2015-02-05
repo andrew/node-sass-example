acc03ae8587c77db4e30c8d28426445aed0eed69
Gregor Martynus <gregor@martynus.net>
2015-02-05 11:36:25 +0100

    fix: update to latest connect version (#2)

--- server.js
+++ server.js
@@ -4,19 +4,20 @@ var sassMiddleware = require('node-sass-middleware');
 var srcPath = __dirname + '/sass';
 var destPath = __dirname + '/public/styles';
 
-var server = connect.createServer(
-    sassMiddleware({
-        src: srcPath,
-        dest: destPath,
-        debug: true,
-        outputStyle: 'expanded',
-        prefix: '/styles'
-    }),
-    connect.static(__dirname + '/public')
+var serveStatic = require('serve-static')
+var http = require('http');
+var port = process.env.PORT || 8000;
+var app = connect();
+app.use('/styles', sassMiddleware({
+  src: srcPath,
+  dest: destPath,
+  debug: true,
+  outputStyle: 'expanded'
+}));
+app.use('/',
+  serveStatic('./public', {})
 );
-
-var port = process.env.PORT || 5000;
-server.listen(port);
+http.createServer(app).listen(port);
 console.log('Server listening on port ' + port);
 console.log('srcPath is ' + srcPath);
 console.log('destPath is ' + destPath);
